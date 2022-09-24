c6eb417c9f14b1c7ba86a717d234e2c4ae6ede46
baran <kevin.baranyizigiye@cashixpay.com>
2022-09-24 22:41:32 +0200

    Linked main activity to PosActivity.kt

--- app/src/main/java/com/gacoca/obr/MainActivity.kt
+++ app/src/main/java/com/gacoca/obr/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.gacoca.obr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var btCaisse: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+        btCaisse.setOnClickListener {
+            val  intent = Intent(this,PosActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun initView(){
+
+        btCaisse = findViewById(R.id.btCaisse)
     }
 }
---
+++ app/src/main/java/com/gacoca/obr/PosActivity.kt
@@ -0,0 +1,12 @@
+package com.gacoca.obr
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PosActivity :AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pos)
+    }
+}
