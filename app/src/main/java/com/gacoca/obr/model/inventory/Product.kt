76a2352b931e53beb5834dc32adcc95106df0e59
baran <kevin.baranyizigiye@cashixpay.com>
2022-10-15 15:46:17 +0200

    CAS-1-Added category & Product Entities

---
+++ app/src/main/java/com/gacoca/obr/model/inventory/Product.kt
@@ -0,0 +1,30 @@
+package com.gacoca.obr.model.inventory
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "product")
+data class Product(
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Int,
+
+    @ColumnInfo(name = "category_name")
+    val categoryName: String,
+
+    @ColumnInfo(name = "product_name")
+    val productName: String,
+
+    @ColumnInfo(name = "product_price")
+    val productPrice: Double,
+
+    @ColumnInfo(name = "product_code")
+    val productCode: String,
+
+    @ColumnInfo(name = "product_unit")
+    val productUnit: String
+
+)
