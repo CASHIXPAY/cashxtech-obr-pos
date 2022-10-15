0103f7a423e3a81d9b61fd6d6e145886ccf958de
baran <kevin.baranyizigiye@cashixpay.com>
2022-10-15 15:44:53 +0200

    Added category & Product Entities

---
+++ app/src/main/java/com/gacoca/obr/model/inventory/Category.kt
@@ -0,0 +1,19 @@
+package com.gacoca.obr.model.inventory
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "category")
+data class Category (
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id :Int,
+
+    @ColumnInfo(name = "category_name")
+    val categoryName: String
+
+
+
+    )
